9a9e7e122516b3ca3a079932cdf0c7d175666d0c
nlawrance <lawrancenicholas@gmail.com>
2019-01-27 11:50:57 +0800

    Order the list of winners consistently

--- sevens.py
+++ sevens.py
@@ -1,15 +1,15 @@
 import game
 import naive_ai as n
 
-ai_list = [n.NaiveAI("Adam"), n.NaiveAI("Bethea"), n.NaiveAI("Catherine"), n.NaiveAI("David")]
-sevens = game.Game(ai_list)
 wins = {}
+ai_list = [n.NaiveAI("Adam"), n.NaiveAI("Bethea"), n.NaiveAI("Catherine"), n.NaiveAI("David")]
+for ai in ai_list:
+    wins[ai.get_name()] = 0
+sevens = game.Game(ai_list)
+
 for i in range(0,10000):
     sevens.reset()
     winner = sevens.play_game()
-    if winner in wins:
-        wins[winner] += 1
-    else:
-        wins[winner] = 1
+    wins[winner] += 1
 
 print(wins)
